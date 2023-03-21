8f7338a29f52bd100692ec1b76d7774fba53de31
MamyAndry <aosimamamisoa@gmail.com>
2023-03-21 08:47:34 +0300

    Reorganisation structure Projet

---
+++ Framework/src/java/etu2060/framework/servlet/FrontServlet.java
@@ -0,0 +1,151 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package etu2060.framework.servlet;
+
+import annotation.AnnotationUrl;
+import framework.Mapping;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class FrontServlet extends HttpServlet {
+    HashMap<String,Mapping> MappingUrls;
+    
+//SETTERS    
+    public void setMappingUrls(HashMap<String, Mapping> MappingUrls){
+        this.MappingUrls = MappingUrls;
+    }
+    
+//GETTERS
+    public HashMap<String, Mapping> getMappingUrls() {
+        return MappingUrls;
+    }
+//METHODS
+    public ArrayList<Class> findClasses(File directory, String packageName) throws ClassNotFoundException {
+        ArrayList<Class> classes = new ArrayList<Class>();
+        if (!directory.exists())
+        {
+            return classes;
+        }
+        File[] files = directory.listFiles();
+        for (File file : files)
+        {
+            if (file.isDirectory())
+            {
+                classes.addAll(findClasses(file, packageName + "." + file.getName()));
+            }
+            else if (file.getName().endsWith(".class"))
+            {
+                classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
+            }
+        }
+        return classes;
+    }
+
+    public ArrayList<Class> getClasses(String packageName) throws ClassNotFoundException, IOException, URISyntaxException{
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        String path = packageName.replace('.', '/');
+        Enumeration<URL> resources = classLoader.getResources(path);
+        ArrayList<File> dirs = new ArrayList<File>();
+        while (resources.hasMoreElements())
+        {
+            URL resource = resources.nextElement();
+            URI uri = new URI(resource.toString());
+            dirs.add(new File(uri.getPath()));
+        }
+        ArrayList<Class> classes = new ArrayList<Class>();
+        System.out.println(dirs);
+        for (File directory : dirs)
+        {
+            classes.addAll(findClasses(directory, packageName));
+        }
+        System.out.println(classes);
+        return classes;
+    }
+
+    public FrontServlet() {
+    }
+
+    @Override
+    public void init() throws ServletException{
+        HashMap<String,Mapping> temp = new HashMap<String,Mapping>();
+        try{
+            ArrayList<Class> list = getClasses("sorona");
+            for(Class element : list){
+               Method[] methods = element.getDeclaredMethods();
+               for(Method m : methods){
+                   if(m.isAnnotationPresent(AnnotationUrl.class)){
+                       AnnotationUrl annotation = m.getAnnotation(AnnotationUrl.class);
+                       temp.put(annotation.url(),new Mapping(element.getName(),m.getName())); 
+                   }
+               }
+            }
+            this.setMappingUrls(temp);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(FrontServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(FrontServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(FrontServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception e){
+            Logger.getLogger(FrontServlet.class.getName()).log(Level.SEVERE, null, e);
+        }  
+    }
+    
+    
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try{
+            String values = request.getRequestURI();
+            out.print(values);
+            out.print("<br>");
+            HashMap<String,Mapping> lst = this.getMappingUrls();
+            out.print(lst);
+//            for(int i = 0 ; i < lst.size() ; i++){
+//                out.print("key = "+lst);
+//                out.print("value = "+lst.get)
+//            }
+        }
+        catch(Exception e){
+            out.print(e.getMessage());
+        }
+    }
+
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ Framework_test/src/java/sorona/Temp2.java
@@ -0,0 +1,12 @@
+package sorona;
+        
+import annotation.AnnotationUrl;
+
+public class Temp2 {
+    int id = 2;
+    
+    @AnnotationUrl(url = "temp2")
+    public int getId(){
+        return id;
+    }
+}
